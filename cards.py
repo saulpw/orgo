e927550e73ed50442e177819580d99e2baf47fe2
Saul Pwanson <saul@pwanson.com>
2012-05-16 01:43:43 -0700

    cleanup before release

--- cards.py
+++ cards.py
@@ -4,8 +4,23 @@ import string
 
 svg = file("card-template.svg").read()
 
-molecules = [
-{
+molecules = {
+1:{
+'name1':"WATER",
+'name2':"",
+'name3':"",
+'type':"",
+'mass':18,
+'state':"liquid",
+'formula':"H2O",
+'flavor':"From a drop of water a logician could infer the possibility of an Atlantic or a Niagara without having seen or heard of one or the other. -- Arthur Conan Doyle",
+'nfpa_blue':0,
+'nfpa_red':0,
+'nfpa_yellow':0,
+'nfpa_extra':"",
+},
+
+3:{
 'name1':"METHANE",
 'name2':"natural gas (80%)",
 'name3':"",
@@ -21,7 +36,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+5:{
 'name1':"ETHANE",
 'name2':"",
 'name3':"",
@@ -37,35 +52,37 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
-'name1':"PROPANE",
-'name2':"cooking gas",
-'name3':"liquified petroleum gas",
-'type':"alkane",
+13:{
+'name1':"OXYGEN",
+'name2':"air (21%)",
+'name3':"",
+'type':"gas",
+'pKa':"",
+'state':"gas",
+'mass':32,
+'formula':"O2",
+'flavor':"Among the notable things about fire is that it also requires oxygen to burn - exactly like its enemy, life. Thereby are life and flames so often compared. -- Otto Weininger",
+'nfpa_blue':0,
+'nfpa_red':0,
+'nfpa_yellow':0,
+'nfpa_extra':"OX",
+},
+10:{
+'name1':"CARBON DIOXIDE",
+'name2':"air (1%)",
+'name3':"",
+'type':"",
+'pKa':"",
 'state':"gas",
 'mass':44,
-'formula':"C3H8",
-'flavor':"Taste the meat, not the heat.",
-'nfpa_blue':1,
-'nfpa_red':4,
+'formula':"CO2",
+'flavor':"As a scientist, my attention became totally focused on global warming some 15 years ago by the elegant and powerful measurements of carbon dioxide trapped in ice cores taken as much as 2 miles deep from the great East Antarctica ice sheet.  -- John Olver",
+'nfpa_blue':2,
+'nfpa_red':0,
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
-'name1':"BUTANE",
-'name2':"liquified petroleum gas",
-'name3':"",
-'type':"alkane",
-'state':"gas",
-'mass':58,
-'formula':"C4H10",
-'flavor':"",
-'nfpa_blue':1,
-'nfpa_red':4,
-'nfpa_yellow':0,
-'nfpa_extra':"",
-},
-{
+16:{
 'name1':"ETHYLENE",
 'name2':"",
 'name3':"",
@@ -79,21 +96,22 @@ molecules = [
 'nfpa_yellow':2,
 'nfpa_extra':"",
 },
-{
-'name1':"ACETYLENE",
-'name2':"welding gas",
+7:{
+'name1':"HYDROGEN PEROXIDE",
+'name2':"",
 'name3':"",
-'type':"alkyne",
-'mass':26,
-'formula':"C2H2",
-'flavor':"",
-'nfpa_blue':1,
-'nfpa_red':4,
-'nfpa_yellow':3,
-'nfpa_extra':"",
+'type':"",
+'pKa':"11.75",
+'state':"liquid",
+'mass':34,
+'formula':"H2O2",
+'flavor':"Bleach, disinfectant, rocket propellant",
+'nfpa_blue':3,
+'nfpa_red':0,
+'nfpa_yellow':2,
+'nfpa_extra':"OX",
 },
-
-{
+9:{
 'name1':"METHANOL",
 'name2':"wood alcohol",
 'name3':"methyl alcohol",
@@ -108,7 +126,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+11:{
 'name1':"ETHANOL",
 'name2':"grain alcohol",
 'name3':"ethyl alcohol",
@@ -123,7 +141,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+20:{
 'name1':"PROPANOL",
 'name2':"1-propyl alcohol",
 'name3':"",
@@ -139,7 +157,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+21:{
 'name1':"ISOPROPYL ALCOHOL",
 'name2':"rubbing alcohol",
 'name3':"2-propanol",
@@ -156,7 +174,7 @@ molecules = [
 'nfpa_extra':"",
 },
 
-{
+17:{
 'name1':"ETHYLENE GLYCOL",
 'name2':"",
 'name3':"",
@@ -171,7 +189,7 @@ molecules = [
 'nfpa_yellow':1,
 'nfpa_extra':"",
 },
-{
+23:{
 'name1':"GLYCEROL",
 'name2':"glycerine",
 'name3':"",
@@ -187,7 +205,7 @@ molecules = [
 'nfpa_extra':"",
 },
 
-{
+15:{
 'name1':"FORMALDEHYDE",
 'name2':"methanal",
 'name3':"formalin",
@@ -202,7 +220,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+6:{
 'name1':"ACETALDEHYDE",
 'name2':"ethyl aldehyde",
 'name3':"ethanal",
@@ -217,7 +235,7 @@ molecules = [
 'nfpa_yellow':2,
 'nfpa_extra':"",
 },
-{
+18:{
 'name1':"ACETONE",
 'name2':"",
 'name3':"",
@@ -233,8 +251,22 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
+19:{
+'name1':"PROPANE",
+'name2':"cooking gas",
+'name3':"liquified petroleum gas",
+'type':"alkane",
+'state':"gas",
+'mass':44,
+'formula':"C3H8",
+'flavor':"Taste the meat, not the heat.",
+'nfpa_blue':1,
+'nfpa_red':4,
+'nfpa_yellow':0,
+'nfpa_extra':"",
+},
 
-{
+4:{
 'name1':"FORMIC ACID",
 'name2':"methanoic acid",
 'name3':"",
@@ -249,7 +281,7 @@ molecules = [
 'nfpa_yellow':1,
 'nfpa_extra':"",
 },
-{
+12:{
 'name1':"ACETIC ACID",
 'name2':"vinegar (10%)",
 'name3':"",
@@ -264,7 +296,7 @@ molecules = [
 'nfpa_yellow':1,
 'nfpa_extra':"",
 },
-{
+24:{
 'name1':"PYRUVIC ACID",
 'name2':"",
 'name3':"",
@@ -279,7 +311,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+22:{
 'name1':"LACTIC ACID",
 'name2':"milk acid",
 'name3':"",
@@ -294,7 +326,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+32:{
 'name1':"MALIC ACID",
 'name2':"",
 'name3':"",
@@ -309,7 +341,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+31:{
 'name1':"OXALOACETIC ACID",
 'name2':"",
 'name3':"",
@@ -324,23 +356,22 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-
-{
-'name1':"CARBON DIOXIDE",
-'name2':"air (1%)",
+36:{
+'name1':"NITROGLYCERIN",
+'name2':"glyceryl trinitrate",
 'name3':"",
-'type':"",
+'type':"nitrate",
 'pKa':"",
-'state':"gas",
-'mass':44,
-'formula':"CO2",
-'flavor':"As a scientist, my attention became totally focused on global warming some 15 years ago by the elegant and powerful measurements of carbon dioxide trapped in ice cores taken as much as 2 miles deep from the great East Antarctica ice sheet.  -- John Olver",
-'nfpa_blue':2,
-'nfpa_red':0,
-'nfpa_yellow':0,
+'state':"liquid",
+'mass':227,
+'formula':"C3H5N3O9",
+'flavor':"Three parts dynamite, with a nitroglycerin cap. It's perfect for small homes, carports and toolsheds. -- Uncle Fester",
+'nfpa_blue':3,
+'nfpa_red':3,
+'nfpa_yellow':4,
 'nfpa_extra':"",
 },
-{
+28:{
 'name1':"ANILINE",
 'name2':"phenylamine",
 'name3':"aminobenzene",
@@ -357,52 +388,37 @@ molecules = [
 'nfpa_extra':"",
 },
 
-{
-'name1':"OXYGEN",
-'name2':"air (21%)",
+29:{
+'name1':"SALICYLIC ACID",
+'name2':"2-hydroxybenzoic acid",
 'name3':"",
-'type':"gas",
-'pKa':"",
-'state':"gas",
-'mass':32,
-'formula':"O2",
-'flavor':"Among the notable things about fire is that it also requires oxygen to burn - exactly like its enemy, life. Thereby are life and flames so often compared. -- Otto Weininger",
-'nfpa_blue':0,
-'nfpa_red':0,
-'nfpa_yellow':0,
-'nfpa_extra':"OX",
-},
-{
-'name1':"HYDROGEN PEROXIDE",
-'name2':"",
-'name3':"",
-'type':"",
-'pKa':"11.75",
-'state':"liquid",
-'mass':34,
-'formula':"H2O2",
-'flavor':"Bleach, disinfectant, rocket propellant",
-'nfpa_blue':3,
-'nfpa_red':0,
-'nfpa_yellow':2,
-'nfpa_extra':"OX",
-},
-{
-'name1':"WATER",
-'name2':"",
-'name3':"",
-'type':"",
-'mass':18,
-'state':"liquid",
-'formula':"H2O",
-'flavor':"From a drop of water a logician could infer the possibility of an Atlantic or a Niagara without having seen or heard of one or the other. -- Arthur Conan Doyle",
-'nfpa_blue':0,
-'nfpa_red':0,
+'type':"analgesic",
+'pKa':"3.5",
+'state':"solid",
+'mass':180,
+'formula':"C9H8O4",
+'flavor':"Ever seen a willow tree with acne?",
+'nfpa_blue':2,
+'nfpa_red':1,
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-
-{
+30:{
+'name1':"ASPIRIN",
+'name2':"acetylsalicylic acid",
+'name3':"",
+'type':"analgesic",
+'pKa':"3.5",
+'state':"solid",
+'mass':180,
+'formula':"C9H8O4",
+'flavor':"",
+'nfpa_blue':2,
+'nfpa_red':1,
+'nfpa_yellow':0,
+'nfpa_extra':"",
+},
+25:{
 'name1':"BENZENE",
 'name2':"",
 'name3':"",
@@ -417,7 +433,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+26:{
 'name1':"TOLUENE",
 'name2':"methylbenzene",
 'name3':"tolulol",
@@ -432,7 +448,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+27:{
 'name1':"PHENOL",
 'name2':"carbolic acid",
 'name3':"phenic acid",
@@ -448,7 +464,7 @@ molecules = [
 'nfpa_extra':"COR",
 },
 
-{
+14:{
 'name1':"NITROGEN",
 'name2':"air (80%)",
 'name3':"",
@@ -463,7 +479,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+8:{
 'name1':"HYDRAZINE",
 'name2':"diamine",
 'name3':"diazane",
@@ -479,7 +495,7 @@ molecules = [
 'nfpa_yellow':3,
 'nfpa_extra':"",
 },
-{
+2:{
 'name1':"AMMONIA",
 'name2':"azane",
 'name3':"hydrogen nitride",
@@ -495,7 +511,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+33:{
 'name1':"HYDROGEN CYANIDE",
 'name2':"formonitrile",
 'name3':"prussic acid",
@@ -510,7 +526,7 @@ molecules = [
 'nfpa_yellow':1,
 'nfpa_extra':"",
 },
-{
+34:{
 'name1':"NITRIC ACID",
 'name2':"aqua fortis",
 'name3':"spirit of niter",
@@ -525,7 +541,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"OX",
 },
-{
+35:{
 'name1':"UREA",
 'name2':"carbamide",
 'name3':"",
@@ -540,7 +556,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+37:{
 'name1':"GLYCINE",
 'name2':"aminoacetic acid",
 'name3':"",
@@ -555,37 +571,20 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
-'name1':"NITROGLYCERIN",
-'name2':"glyceryl trinitrate",
+38:{
+'name1':"ACETYLENE",
+'name2':"welding gas",
 'name3':"",
-'type':"nitrate",
-'pKa':"",
-'state':"liquid",
-'mass':227,
-'formula':"C3H5N3O9",
-'flavor':"Three parts dynamite, with a nitroglycerin cap. It's perfect for small homes, carports and toolsheds. -- Uncle Fester",
-'nfpa_blue':3,
-'nfpa_red':3,
-'nfpa_yellow':4,
-'nfpa_extra':"",
-},
-{
-'name1':"ASPIRIN",
-'name2':"acetylsalicylic acid",
-'name3':"",
-'type':"analgesic",
-'pKa':"3.5",
-'state':"solid",
-'mass':180,
-'formula':"C9H8O4",
+'type':"alkyne",
+'mass':26,
+'formula':"C2H2",
 'flavor':"",
-'nfpa_blue':2,
-'nfpa_red':1,
-'nfpa_yellow':0,
+'nfpa_blue':1,
+'nfpa_red':4,
+'nfpa_yellow':3,
 'nfpa_extra':"",
 },
-{
+39:{
 'name1':"BIPHENYL",
 'name2':"",
 'name3':"",
@@ -600,7 +599,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+40:{
 'name1':"ACETAMINOPHEN",
 'name2':"",
 'name3':"",
@@ -615,7 +614,7 @@ molecules = [
 'nfpa_yellow':0,
 'nfpa_extra':"",
 },
-{
+41:{
 'name1':"TRINITROTOLUENE",
 'name2':"TNT",
 'name3':"",
@@ -630,7 +629,7 @@ molecules = [
 'nfpa_yellow':4,
 'nfpa_extra':"",
 },
-]
+}
 
 g_fp = None
 nSheets = 0
@@ -652,7 +651,8 @@ def out(s):
 #"""
 f = string.Formatter()
 nCards=0
-for m in molecules:
+for i in molecules.keys():
+    m = molecules[i]
     dy = [ ]
     subscript = False
     for x in m["formula"]:
